d332da16fd7ab41f967e743e492db0ff24384911
requestjie@163.com <requestjie@163.com>
2020-06-16 17:34:54 +0800

    下班提交代码

--- user-center/src/main/java/com/xiaopang/usercenter/UserCenterApplication.java
+++ user-center/src/main/java/com/xiaopang/usercenter/UserCenterApplication.java
@@ -2,7 +2,9 @@ package com.xiaopang.usercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan("com.xiaopang")
 @SpringBootApplication
 public class UserCenterApplication {
 
