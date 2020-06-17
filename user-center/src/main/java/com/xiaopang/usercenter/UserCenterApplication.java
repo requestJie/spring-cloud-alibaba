5282e4601c297dca43470ca1bccdcec37000f185
requestjie@163.com <requestjie@163.com>
2020-06-17 14:06:46 +0800

    整合通用mapper

--- user-center/src/main/java/com/xiaopang/usercenter/UserCenterApplication.java
+++ user-center/src/main/java/com/xiaopang/usercenter/UserCenterApplication.java
@@ -3,7 +3,7 @@ package com.xiaopang.usercenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
-
+//扫描mybatis哪些包里面的接口
 @MapperScan("com.xiaopang")
 @SpringBootApplication
 public class UserCenterApplication {
